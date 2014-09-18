7eb207260b4cbaa94b2fb032415393fbf2023412
sjoulbak <remco_meinen@hotmail.com>
2014-09-18 10:27:29 +0200

    save system #3

---
+++ Core.rb
@@ -0,0 +1,15 @@
+def clear
+	system "cls"
+	system "clear"
+end
+def write_at(fname, at_line, sdat)
+  open(fname, 'r+') do |f|
+    while (at_line-=1) > 0          # read up to the line you want to write after
+      f.readline
+    end
+    pos = f.pos                     # save your position in the file
+    rest = f.read                   # save the rest of the file
+    f.seek pos                      # go back to the old position
+    f.write [sdat, rest]            # write new data & rest of file
+  end
+end
